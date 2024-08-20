3f7b60fef73105dc97a0531616d19302d4fabb02
NeemaMwende <gracepete12@gmail.com>
2024-08-20 16:02:16 +0300

    if statement

---
+++ decision-making.dart
@@ -0,0 +1,9 @@
+//If Statement
+// if statement outputs depending on a certain conditional expression
+void main() {
+  var age = 30;
+  if (age > 18) {
+    print("Mariam is a voter in Kenya");
+  }
+  //print("Mariam is still young to vote");
+}
